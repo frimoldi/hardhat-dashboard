bd5b7a6a0e0a7215c2799b599fe7db17ca43d5e0
Fran Rimoldi <francisco.rimoldi@gmail.com>
2022-06-10 09:41:48 +0200

    feat: add basic chackra-ui layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Container, Flex } from "@chakra-ui/react"
+
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Flex
+      background="orange"
+      align="center"
+      justify="space-evenly"
+      height="100vh"
+    >
+      <Container padding={0}>
+        <Flex direction="column" gap={10}>
+          <Flex grow={1} background="white" borderRadius={20} padding={5}>
+            Current block:
+          </Flex>
+        </Flex>
+      </Container>
+    </Flex>
+  )
 }
 
-export default App;
+export default App
